7f6aa78513f74e8a137b95fa1f024bcee1e53438
JeaSung Lee <wotjdzz1@naver.com>
2019-10-11 13:01:00 +0900

    가로스크롤 제한

--- Source/Content/SegementSlideContentView.swift
+++ Source/Content/SegementSlideContentView.swift
@@ -29,7 +29,11 @@ public class SegementSlideContentView: UIView {
     private var initSelectedIndex: Int?
     
     public private(set) var selectedIndex: Int?
-    public var isVerticalScrollable: Bool = true
+    public var isVerticalScrollable: Bool = true {
+        willSet {
+            scrollView.isPagingEnabled = newValue
+        }
+    }
     public weak var delegate: SegementSlideContentDelegate?
     public weak var viewController: UIViewController?
     
