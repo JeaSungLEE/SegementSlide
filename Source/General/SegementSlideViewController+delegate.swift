920b10e2bfa6685b127ad0c77a6df93309222cf2
JeaSung Lee <wotjdzz1@naver.com>
2019-12-16 15:59:58 +0900

    resetobservation add

--- Source/General/SegementSlideViewController+delegate.swift
+++ Source/General/SegementSlideViewController+delegate.swift
@@ -73,6 +73,18 @@ extension SegementSlideViewController: SegementSlideContentDelegate {
 }
 
 extension SegementSlideViewController {
+    public func resetScrollViewObservation(scrollView: UIScrollView) {
+        childKeyValueObservation?.invalidate()
+
+        let keyValueObservation = scrollView.observe(\.contentOffset, options: [.new, .old], changeHandler: { [weak self] (scrollView, change) in
+            guard let self = self else { return }
+            guard change.newValue != change.oldValue else { return }
+            self.childScrollViewDidScroll(scrollView)
+        })
+
+        childKeyValueObservation = keyValueObservation
+    }
+
     public func resetScrollViewObservation() {
         childKeyValueObservation?.invalidate()
         
